82e07df597df6e0d4e9b408e56373f6b3f496555
Tamara Jarvis <75946134+jarvistamara@users.noreply.github.com>
2021-06-04 00:26:22 -0400

    working on Class form

--- src/containers/ClassForm.js
+++ src/containers/ClassForm.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
-const ClassForm = () => {
+const ClassForm = ({addANewClassroom}) => {
+    const [title, setTitle] = useState('')
+    const [teacherName, setTeacherName] = useState('')
+
+  const handleChange = (e) => {
+        setTitle(e.target.value)
+        setTeacherName(e.target.value)
+    }
+
+  const handleSubmit = (e) => {
+        e.preventDefault()
+        addANewClassroom(title, teacherName)
+    }
+
     return (
         <div>
             <h1 className='class-form'>Classroom Form</h1>
             <hr />
-
+            <form onSubmit={handleSubmit}>
+                <label>Title:  </label>
+                <input type='text' title='title' value='title' onChange={handleChange}/>
+                <label>Teacher's Name:  </label>
+                <input type='text' teacherName='teacher_name' value='teacherName' onChange={handleChange}/>
+                <br />
+                <input type='submit'/>
+            </form>
         </div>
     )
 }
--- src/containers/Classrooms.js
+++ src/containers/Classrooms.js
@@ -15,10 +15,6 @@ const Classrooms = () => {
             setClassrooms(data))
     }, [])
 
-    const formToggle = (e) => {
-        console.log(e.target)
-        setClassForm(true)
-    }
     const classroomsList = classrooms.map(c => 
     <Link to={`/classrooms/${c.id}`}>
         <li key={c.id} classname="classlist-li">
@@ -26,15 +22,39 @@ const Classrooms = () => {
             </li>
             </Link>)
 
+    const changeFormState = (e) => {
+        console.log(e.target)
+        setClassForm(true)
+    }
+
+    const addNewClassroom = (classroom) => {
+       const headerConfig = {
+           method: 'POST',
+           headers: {'Content-Type': 'application/json'},
+           body: JSON.stringify(classroom)
+        }
+        fetch('http://localhost:9292/classrooms', headerConfig)
+        .then(res => res.json())
+        .then(data => {
+            setClassrooms([...classrooms, data])
+        })
+        setClassForm(false)
+    }
+
     return (
         <div>
             <h1>Current Classrooms</h1>
             <img src={image1} alt="Kindergarden class"/>
+            <br/>
+            <br />
+            {classForm ? <ClassForm addANewClassroom={addNewClassroom}/> : <button onClick={changeFormState}>Add New Classroom</button>}
+            <br />
+            <br />
             <p>Click one a Class below to view Class details and Students.</p>
             <ol className='classlist-li'>
                <h4>{classroomsList}</h4> 
             </ol>
-            {classForm ? <ClassForm /> : <button onClick={formToggle}>Add New Classroom</button>}
+            
         </div>
     )
 }
